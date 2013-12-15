93c7211311d77d7e591051be756a6a5bcdd7e88c
Mark Schipper <schipperius@gmail.com>
2013-12-15 14:08:06 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cabbage::Application.config.secret_key_base = '66a2ab607e201bd66db1c50c4649c2f319c30c6332551f6a4702d389c1228178925a1d1185e1d710441651cb6d8365d17b4878c7caa77e4a5565499cb429e726'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cabbage::Application.config.session_store :cookie_store, key: '_cabbage_session'
